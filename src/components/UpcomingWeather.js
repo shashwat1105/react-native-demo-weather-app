be7db959afba74267493e444cce4523b1f218215
shashwat1105 <sharmashashwat634@gmail.com>
2023-08-24 23:25:12 +0530

    secondCommit

---
+++ src/components/UpcomingWeather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView,StyleSheet,Text } from "react-native";
+
+
+const UpcomingWeather=()=>{
+    return(
+<SafeAreaView styles={StyleSheet.container}>
+    <Text>Upcoming Weather</Text>
+</SafeAreaView>
+    )
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1
+    }
+})
